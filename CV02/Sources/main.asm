cfd98c8fefc8d13c52cd19975c75438483f72c89
Jiří Šrámek <jiri.sramek.g@gmail.com>
2018-03-18 16:47:14 +0100

    Added readme, .gitignore and CV02

---
+++ CV02/Sources/main.asm
@@ -0,0 +1,73 @@
+; The MIT License (MIT)
+;
+; Copyright (c) 2018 Jiri Sramek (https://github.com/JiriS97/)
+;
+; Permission is hereby granted, free of charge, to any person obtaining a copy
+; of this software and associated documentation files (the "Software"), to deal
+; in the Software without restriction, including without limitation the rights
+; to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+; copies of the Software, and to permit persons to whom the Software is
+; furnished to do so, subject to the following conditions:
+;
+; The above copyright notice and this permission notice shall be included in
+; all copies or substantial portions of the Software.
+;
+; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+; IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+; FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+; AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+; LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+; OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+; THE SOFTWARE.
+
+; Include derivative-specific definitions
+            INCLUDE 'derivative.inc'
+            
+;
+; export symbols
+;
+            XDEF _Startup
+            ABSENTRY _Startup
+
+;
+; variable/data section
+;
+            ORG    RAMStart		; Insert your data definition here
+ExampleVar: DS.B   1
+
+;
+; code section
+;
+            ORG    ROMStart
+            
+
+_Startup:
+            LDHX   #RAMEnd+1    ; initialize the stack pointer
+            TXS
+            CLI					; enable interrupts
+
+mainLoop:
+            ; Insert your code here
+            NOP
+
+            feed_watchdog
+            BRA    mainLoop
+			
+;**************************************************************
+;* spurious - Spurious Interrupt Service Routine.             *
+;*             (unwanted interrupt)                           *
+;**************************************************************
+
+spurious:				; placed here so that security value
+			NOP			; does not change all the time.
+			RTI
+
+;**************************************************************
+;*                 Interrupt Vectors                          *
+;**************************************************************
+
+            ORG	$FFFA
+
+			DC.W  spurious			;
+			DC.W  spurious			; SWI
+			DC.W  _Startup			; Reset
---
+++ README.md
@@ -0,0 +1,16 @@
+# BMIC Projects
+This repository is a collection of programs written by me on BMIC lessons on BUT (VUT Brno in Czech).
+These programs are written in Assembler/C, task description is included in each main file, when possible.
+All projects are for HCS08 MCU family 
+
+## Getting Started
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes. 
+
+### Prerequisites
+To compile these projects yourself you will need CodeWarrior, you can get a free copy from NXP's website.
+Some of the projects contain more than one main file, usually named main and mainB, that is because
+the exercise had two parts, you need to exclude the file you don't need from build
+(right click -> Resource configuration -> Exclude from build)
+ 
+## License
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for detail.
